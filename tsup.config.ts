3cdf4f86690091598b2fc6ee13a39b3e0a78fd7e
ɛʟʟɨօȶ b  =^._.^= ∫ <elliot@email.gd>
2025-04-21 21:32:43 -0400

    ♻️ (tsup.config.ts): refactor build configuration to streamline options and improve readability by consolidating common settings into a base configuration.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,59 +2,53 @@ import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
 
-const opts: Options = {
-  name: "rita",
+const base: Partial<Options> = {
   entry: { rita: 'src/rita.js' },
   outDir: 'dist',
-  watch: false,
   clean: true,
-  minify: false,
-  sourcemap: true,
-  dts: false,
+  target: 'esnext',
   bundle: true,
-  target: 'es2020',
-  metafile: false, // toggle for stats: https://esbuild.github.io/analyze/
+  treeshake: true,
+  minifySyntax: true,
   esbuildPlugins: [esbuildPluginVersionInjector()],
-  outExtension({ format }) { return { js: `.js` } },
-}
+  outExtension({ format }) {
+    return { js: format === 'cjs' ? '.cjs' : '.js' };
+  },
+};
 
 const esm: Options = {
+  ...base,
   format: ['esm'],
-  ...opts,
-}
+};
 
 const cjs: Options = {
+  ...base,
   format: ['cjs'],
-  ...opts,
-  platform: "node",
+  platform: 'node',
   cjsInterop: true,
-  splitting: true,
-  outExtension({ format }) { return { js: `.cjs` } },
-}
+};
 
 const iife: Options = {
+  ...base,
   format: ['iife'],
-  ...opts,
   minify: true,
-  platform: "browser",
-  globalName: "iife",
-  footer: { js: "RiTa = iife.RiTa" },
-  outExtension({ format }) { return { js: `.min.js` } },
-
-}
+  platform: 'browser',
+  globalName: 'iife',
+  footer: { js: 'RiTa = iife.RiTa' },
+  outExtension() {
+    return { js: '.min.js' };
+  },
+};
 
 const testEsm: Options = {
   format: ['esm'],
-  platform: "node",
-  name: "test",
+  platform: 'node',
+  name: 'test',
   entry: ['test/[^i]*.js'],
   outDir: 'test/dist',
-  watch: false,
   clean: false,
   minify: false,
-  sourcemap: false,
-  dts: false,
   bundle: false,
-}
+};
 
 export default defineConfig([esm, cjs, iife, testEsm]);
